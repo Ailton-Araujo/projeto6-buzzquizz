065c45c9e6ed08c1afc74cfaeb960af192b9bb7b
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-04-20 17:54:22 -0300

    Adicionar perguntas

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,74 @@
-const userQuizz ={
+const userQuizz = {
     title: "",
     image: "",
     questions: [],
     levels: []
-}
+};
 
-function addQuizz(){
+const level = {
+    title: "",
+    image: "",
+    text: "",
+    minValue: Number
+};
+
+function addQuizz() {
     document.querySelector(".home-page").classList.add("hidden");
     document.querySelector(".addQuizz.hidden").classList.remove("hidden");
-}
+};
 
 /* Função para checar se a URL é valida*/
-function checkUrl (string) {
+function checkUrl(string) {
     let givenURL;
     try {
-        givenURL = new URL (string);
+        givenURL = new URL(string);
     } catch (error) {
-        console.log ("error is", error);
-       return false; 
+        console.log("error is", error);
+        return false;
     }
     return true;
-  }
+}
 
-function renderUserQuestions(object){
+/* Função para checar se a cor está em um formato HEX valido*/
+function isHexColor(hex) {
+    let isHex = hex.match(/[A-F-0-9]/gi);
+    if (hex[0] === "#" &&
+        isHex.length === 6) {
+        console.log(isHex)
+        return true;
+    }
+    return false;
+}
+
+function openQuestion(element) {
+    let questionOpened = document.querySelector(".opened");
+    questionOpened.classList.remove("opened");
+    questionOpened.classList.add("closed");
+    questionOpened.previousElementSibling.innerHTML += `
+    <button type="button" onclick="openQuestion(this)"><img src="./assets/button-img.png" alt=""></button>`
+
+    element.parentNode.nextElementSibling.classList.remove("closed");
+    element.parentNode.nextElementSibling.classList.add("opened");
+    element.remove();
+}
+
+
+function renderUserQuestions(object) {
+    let displayStatus = "";
     const questionsFront = document.querySelector(".questionsQuizz > .questions");
     questionsFront.innerHTML = ""
-    for(let i = 0; i<object.questions.length;i++){
+    for (let i = 0; i < object.questions.length; i++) {
+        if (i === 0) {
+            displayStatus = "opened";
+        } else {
+            displayStatus = "closed";
+        };
         questionsFront.innerHTML += `
-        <ul data-test="question-ctn" class="question inputs">
-            <h3>Pergunta ${i+1}</h3>
+        <ul data-test="question-ctn" class="question${i + 1} inputs">
+            <div class="questionButton">
+            <h3>Pergunta ${i + 1}
+            </div>
+            <div class="${displayStatus}">
             <span class="space"></span>
             <li><input data-test="question-input" class="textQuestion" type="text" placeholder="Texto da pergunta" /></li>
             <li><input data-test="question-color-input" class="colorQuestion" type="text"
@@ -59,11 +99,20 @@ function renderUserQuestions(object){
                 placeholder="Resposta incorreta 3" />
             </li>
             <li><input data-test="wrong-img-input" class="wrongImg3" type="text" placeholder="URL da imagem 3" /></li>
-        </ul>`
+        </ul>
+        </div>`
+        if (i > 0) {
+            questionsFront.querySelector(`.question${i + 1} .closed`).previousElementSibling.innerHTML += `
+            <button type="button" onclick="openQuestion(this)"><img src="./assets/button-img.png" alt=""></button>`
+        }
     };
 }
 
-function toQuestions(){
+function renderUserLevel() {
+
+}
+
+function toQuestions() {
     const inputTitle = document.querySelector(".quizzTitle");
     const inputImg = document.querySelector(".quizzImg");
     const inputNQuestions = document.querySelector(".nquestions");
@@ -71,12 +120,12 @@ function toQuestions(){
 
     const titleFront = document.querySelector(".titleQuizz").classList;
     const questionsFront = document.querySelector(".questionsQuizz.hidden").classList;
-    
-    if((inputTitle.value.length >=20 && inputTitle.value.length <=65) &&
-        (checkUrl (inputImg.value)) &&
-        (inputNQuestions.value>2 && Number.isInteger(Number(inputNQuestions.value))) &&
-        (inputNLevels.value>1 && Number.isInteger(Number(inputNLevels.value)))
-    ){
+
+    if ((inputTitle.value.length >= 20 && inputTitle.value.length <= 65) &&
+        (checkUrl(inputImg.value)) &&
+        (inputNQuestions.value > 2 && Number.isInteger(Number(inputNQuestions.value))) &&
+        (inputNLevels.value > 1 && Number.isInteger(Number(inputNLevels.value)))
+    ) {
         userQuizz.title = inputTitle.value;
         userQuizz.image = inputImg.value;
         userQuizz.questions.length = inputNQuestions.value;
@@ -84,18 +133,85 @@ function toQuestions(){
 
         inputTitle.value = "";
         inputImg.value = "";
-        inputNQuestions.value= "";
+        inputNQuestions.value = "";
         inputNLevels.value = "";
 
         titleFront.add("hidden");
         questionsFront.remove("hidden");
 
         renderUserQuestions(userQuizz);
-    }else{
+    } else {
         alert("Por favor preencha os dados corretamente.")
         inputTitle.value = "";
         inputImg.value = "";
-        inputNQuestions.value= "";
+        inputNQuestions.value = "";
         inputNLevels.value = "";
     }
+}
+
+function toLevels() {
+
+    for (let i = 0; i < userQuizz.questions.length; i++) {
+        let question = {
+            title: "",
+            color: "",
+            answers: []
+        };
+
+        const inputQuestion = document.querySelector(`.question${i + 1} .textQuestion`);
+        const inputColor = document.querySelector(`.question${i + 1} .colorQuestion`);
+
+        if (inputQuestion.value.length >= 20 &&
+            isHexColor(inputColor.value)) {
+            question.title = inputQuestion.value;
+            question.color = inputColor.value;
+        } else {
+            alert("Por favor preencha os dados corretamente.")
+            inputQuestion.value = "";
+            inputColor.value = "";
+            return
+        }
+
+        const inputCorrectAnswer = document.querySelector(`.question${i + 1} .correctAnswer`);
+        const inputCorrectImg = document.querySelector(`.question${i + 1} .correctImg`);
+
+        if (inputCorrectAnswer.value !== "" &&
+            (checkUrl(inputCorrectImg.value))) {
+            let answer = {};
+            answer.text = inputCorrectAnswer.value;
+            answer.image = inputCorrectImg.value;
+            answer.isCorrectAnswer = true;
+            question.answers.push(answer);
+        } else {
+            alert("Por favor preencha os dados corretamente.")
+            inputCorrectAnswer.value = "";
+            inputCorrectImg.value = "";
+            return
+        }
+        for (let k = 1; k < 4; k++) {
+            const inputWrongAnswer = document.querySelector(`.question${i + 1} .wrongAnswer${k}`);
+            const inputWrongImg = document.querySelector(`.question${i + 1} .wrongImg${k}`);
+            if (inputWrongAnswer.value !== "" &&
+                (checkUrl(inputWrongImg.value))) {
+                let answer = {};
+                answer.text = inputWrongAnswer.value;
+                answer.image = inputWrongImg.value;
+                answer.isCorrectAnswer = false;
+                question.answers.push(answer);
+            } else if (k === 1) {
+                alert("Por favor preencha os dados corretamente.")
+                inputWrongAnswer.value = "";
+                inputWrongImg.value = "";
+                return;
+            }
+        }
+        userQuizz.questions[i] = question;
+    }
+    console.log(userQuizz)
+    const questionsFront = document.querySelector(".questionsQuizz").classList;
+    const levelFront = document.querySelector(".levelQuizz.hidden").classList;
+    questionsFront.add("hidden");
+    levelFront.remove("hidden");
+
+    renderUserLevel()
 }
